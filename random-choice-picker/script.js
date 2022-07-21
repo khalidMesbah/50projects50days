d464c495c9a56b6fb48debd07fbaea6006c427a3
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-21 22:44:41 +0200

    finish

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,69 +1,59 @@
-const tagsEl = document.getElementById('tags')
-const textarea = document.getElementById('textarea')
+const textarea = document.getElementById(`textarea`);
+const tagsContainer = document.getElementById(`tags`);
+const MAXIMUM_NUMBER_OF_CHOICES = 10;
 
-textarea.focus()
+let currentChoice = ``;
+let startIndex = 0;
+let endIndex = 0;
+textarea.addEventListener(`input`, () => {
+    const value = textarea.value;
+    if(value.trim().length === 0) tagsContainer.innerHTML = ``
+    endIndex = value.length
+    currentChoice = value.slice(startIndex, endIndex);
+    
+    if (currentChoice.at(-1) === `,`) {
+        const res = currentChoice.slice(0,-1)
+        if (res !== ``) createChoice(res);
+        startIndex += currentChoice.length;
+    }
+});
 
 textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
-
-    if(e.key === 'Enter') {
-        setTimeout(() => {
-            e.target.value = ''
-        }, 10)
-
-        randomSelect()
+    const tagsLength = [...document.querySelectorAll(`.tag`)].length;
+    if (e.key === `Enter` && tagsLength >= 1) {
+        createChoice(textarea.value.slice(startIndex, endIndex))
+        getRandomChoice();
+        textarea.blur();
     }
-})
+});
 
-function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-    
-    tagsEl.innerHTML = ''
+textarea.focus();
 
-    tags.forEach(tag => {
-        const tagEl = document.createElement('span')
-        tagEl.classList.add('tag')
-        tagEl.innerText = tag
-        tagsEl.appendChild(tagEl)
-    })
-}
-
-function randomSelect() {
-    const times = 30
-
+const getRandomChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 10;
+    const tags = [...document.querySelectorAll(`.tag`)];
+    let counter = 0;
+    let i = 0;
     const interval = setInterval(() => {
-        const randomTag = pickRandomTag()
-	
-	if (randomTag !== undefined) {
-        highlightTag(randomTag)
+        (i === tags.length - 1) ? i = 0 : i++;
+        tags[i].classList.add(`highlight`);
+        (i) ? tags[i - 1].classList.remove(`highlight`) : tags[tags.length - 1].classList.remove(`highlight`);
+        counter++;
+        if (counter === randomNumber) clearInterval(interval);
+    }, 150);
+};
 
-        setTimeout(() => {
-            unHighlightTag(randomTag)
-        }, 100)
-	}
-    }, 100);
-
-    setTimeout(() => {
-        clearInterval(interval)
-
-        setTimeout(() => {
-            const randomTag = pickRandomTag()
-
-            highlightTag(randomTag)
-        }, 100)
-
-    }, times * 100)
-}
-
-function pickRandomTag() {
-    const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
-}
-
-function highlightTag(tag) {
-    tag.classList.add('highlight')
-}
-
-function unHighlightTag(tag) {
-    tag.classList.remove('highlight')
-}
+const createChoice = (choice) => {
+    const tags = document.querySelectorAll(`.tag`);
+    // prevent exceeding the MAXIMUM_NUMBER_OF_CHOICES
+    if (tags.length === MAXIMUM_NUMBER_OF_CHOICES) return;
+    // prevent same choices
+    const choiceExists = [...tags].some(tag => tag.textContent === choice);
+    if (choiceExists) return;
+    // create a new choice
+    const tag = document.createElement(`div`);
+    tag.classList.add(`tag`);
+    const innerText = document.createTextNode(choice);
+    tag.appendChild(innerText);
+    tagsContainer.appendChild(tag);
+};
