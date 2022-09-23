088ae5b04bf46981d72c205e707c8a146a5e824f
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-09-23 17:42:38 +0200

    finish form input wave

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,8 @@
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll(".form-control label");
 
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
-})
+labels.forEach((label) => {
+  label.innerHTML = label.innerHTML
+    .split("")
+    .map((e, i) => `<span style="transition-delay:${i * 50}ms">${e}</span>`)
+    .join("");
+});
