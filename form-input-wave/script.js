648af2e1d2cf044fffc3262ba20101ea5c6fef7f
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-09-24 14:01:05 +0200

    fix from input

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,7 @@
-const labels = document.querySelectorAll(".form-control label");
-
-labels.forEach((label) => {
-  label.innerHTML = label.innerHTML
-    .split("")
-    .map((e, i) => `<span style="transition-delay:${i * 50}ms">${e}</span>`)
-    .join("");
-});
+document.querySelectorAll("label").forEach(
+  (label) =>
+    (label.innerHTML = label.innerHTML
+      .split("")
+      .map((e, i) => `<span style="transition-delay:${i * 50}ms">${e}</span>`)
+      .join(""))
+);
