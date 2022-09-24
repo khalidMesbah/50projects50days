9bee83e96c8f541998ce11ababe558243afd996e
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-09-24 14:00:55 +0200

    finish 3d boxes background

--- 3d-boxes-background/script.js
+++ 3d-boxes-background/script.js
@@ -1,17 +1,18 @@
-const boxesContainer = document.getElementById('boxes')
-const btn = document.getElementById('btn')
+const boxesContainer = document.getElementById(`boxes`);
+const btn = document.getElementById("btn");
 
-btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
 
 function createBoxes() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const box = document.createElement('div')
-      box.classList.add('box')
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-      boxesContainer.appendChild(box)
-    }
+  const fragment = document.createDocumentFragment();
+  for (let i = 0, j = -1; i < 16; i++) {
+    i % 4 == 0 && j++;
+    const box = document.createElement(`div`);
+    box.className = `box`;
+    box.style.backgroundPosition = `-${i * 100}% -${j * 100}%`;
+    fragment.appendChild(box);
   }
+  boxesContainer.appendChild(fragment);
 }
 
-createBoxes()
+createBoxes();
